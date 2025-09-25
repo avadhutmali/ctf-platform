c7512031403b2a4099bc2982ec7e240f13dfc553
avadhutmali <mali.avadhut7@gmail.com>
2025-09-25 12:52:00 +0530

    config changes

--- ctf-platform-frontend/src/config.js
+++ ctf-platform-frontend/src/config.js
@@ -1,3 +1,4 @@
-export const API_BASE_URL = 'https://ctf-platform-production-1c45.up.railway.app';
+// export const API_BASE_URL = 'https://ctf-platform-production-1c45.up.railway.app';
+export const API_BASE_URL = 'http://localhost:8080';
 
 
